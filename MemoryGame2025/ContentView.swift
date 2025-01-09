3dd36cf975d60d6c9137181f90cf08d58bc4e6f1
Alex <515260@camosun>
2025-01-08 16:48:58 -0800

    Lab 1

--- MemoryGame2025/ContentView.swift
+++ MemoryGame2025/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var array = ["square.and.arrow.up", "square.and.arrow.up.fill", "square.and.arrow.up.circle", "square.and.arrow.up.circle.fill"]
+    @State var index = 1
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            Button(
+                action:{
+                    if (index <= 0) {
+                        index = 3
+                    } else {
+                        index -= 1
+                    }
+                }) {
+                Image(systemName: "arrowtriangle.left")
+                    .resizable()
+                    .frame(width: 70, height: 60)
+            }
+            Image(systemName: array[index])
+                .resizable()
+                .frame(width:100, height:100)
+                .aspectRatio(0.75, contentMode: .fit)
+            Button(
+                action:{
+                    if (index >= 3) {
+                        index = 0
+                    } else {
+                        index += 1
+                    }
+                }) {
+                Image(systemName: "arrowtriangle.right")
+                    .resizable()
+                    .frame(width: 70, height: 60)
+            }
         }
         .padding()
+        	
     }
 }
 
