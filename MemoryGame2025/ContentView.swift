52d0e89186694faceeb9b8dbf4ac8dbd778984fb
Alex <515260@camosun>
2025-01-15 15:22:53 -0800

    Moved Image HStack to ImagePickerView, added UserDefaults to state variables

--- MemoryGame2025/ContentView.swift
+++ MemoryGame2025/ContentView.swift
@@ -10,35 +10,57 @@ import SwiftUI
 struct ContentView: View {
     @State var array = ["square.and.arrow.up", "square.and.arrow.up.fill", "square.and.arrow.up.circle", "square.and.arrow.up.circle.fill"]
     @State var index = 1
+    @State private var bonus = UserDefaults.standard.value(forKey:"BonusTile") as? Bool ?? false
+    @State private var rows = UserDefaults.standard.value(forKey:"Rows") as? Int ?? 2
+    @State private var cols = UserDefaults.standard.value(forKey:"Cols") as? Int ?? 2
+    @State private var tiles = UserDefaults.standard.value(forKey:"Tiles") as? Int ?? 2
     var body: some View {
-        HStack {
-            Button(
-                action:{
-                    index = (index <= 0) ? 3 : index - 1
-                }) {
-                Image(systemName: "arrowtriangle.left")
-                    .resizable()
-                    .frame(width: 75, height: 60)
+        NavigationStack {
+            ImagePickerView(array: $array, index: $index)
+            VStack{
+                HStack{
+                    Text(String(rows))
+                    Stepper("Rows",
+                            value: Binding(get:
+                                            { rows }, set:
+                                            { newValue in
+                        rows = newValue
+                        UserDefaults.standard.set(rows, forKey:"Rows")}), in: 2...10)
+                }
+                HStack {
+                    Text(String(cols))
+                    Stepper("Columns", value: Binding(get:
+                                                        { cols }, set:
+                                                        { newValue in
+                                    cols = newValue
+                                    UserDefaults.standard.set(cols, forKey:"Cols")}), in: 2...10)
+                }
+                HStack{
+                    Text(String(tiles))
+                    Stepper("Tiles", value: Binding(get:
+                                                        { tiles }, set:
+                                                        { newValue in
+                                    tiles = newValue
+                                    UserDefaults.standard.set(tiles, forKey:"Tiles")}), in: 2...10)
+                }
+                Toggle("Bonus Tile", isOn: Binding(get: { bonus }, set: { newValue in
+                    bonus = newValue
+                    UserDefaults.standard.set(bonus, forKey: "BonusTile")
+                }))
+                    .toolbar {
+                        ToolbarItemGroup {
+                            Button(action: {
+                                
+                            }, label: {
+                                Image(systemName: "gear")
+                            })
+                        }
+                    }
             }
-            Spacer()
-            Image(systemName: array[index])
-                .resizable()
-                .frame(width:100, height:100)
-                .aspectRatio(0.75, contentMode: .fit)
-            Spacer()
-            Button(
-                action:{
-                    index = (index >= 3) ? 0 : index + 1
-                }) {
-                Image(systemName: "arrowtriangle.right")
-                    .resizable()
-                    .frame(width: 75, height: 60)
+            .padding()
             }
         }
-        .padding()
-        	
     }
-}
 
 #Preview {
     ContentView()
