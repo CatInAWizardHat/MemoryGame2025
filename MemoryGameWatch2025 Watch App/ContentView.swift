e3ed89effc3896a9650e5cd1e8777df33e6c0943
Alex <515260@camosun>
2025-03-19 13:46:00 -0700

    Add watchOS target

---
+++ MemoryGameWatch2025 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoryGameWatch2025 Watch App
+//
+//  Created by IMD 224 on 2025-03-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
